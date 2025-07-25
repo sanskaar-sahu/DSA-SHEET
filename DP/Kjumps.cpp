5bc3b103c1efccb18ebf71dc9f8094184209a4cd
sanskaar-sahu <sanskarsahu2095@gmail.com>
2025-07-25 23:40:22 +0530

    Optimised solution is added using tabulation

--- DP/Kjumps.cpp
+++ DP/Kjumps.cpp
@@ -18,6 +18,24 @@ public:
         }
         return minJumps;
     }
+    
+    int optimizedKjumps(vector<int>& heights, int k){
+        int n = heights.size() ;
+        vector<int>dp(n +1  , -1);
+        dp[0] = 0;
+        for(int i = 1 ; i < n ; i++){
+            int minJumps = INT_MAX;
+            for(int j = 1 ; j <= k; j++){
+                if((i - j) >= 0){
+                    int jump = dp[i -j] + abs(heights[i] - heights[i - j]);
+                    minJumps = min(minJumps , jump);
+                }
+                dp[i] = minJumps;
+            }
+        }
+       return dp[n-1];
+    }
+    
     int frogJump(vector<int>& heights, int k) {
         dp[0] = 0;
         int n = heights.size()
