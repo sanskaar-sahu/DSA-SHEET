a9b755e1e2d54019e93bb9f54447c202982c98f7
sanskaar-sahu <sanskarsahu2095@gmail.com>
2025-08-06 18:25:50 +0530

    Length of longest common substring

---
+++ DP/String/LCsubstring.cpp
@@ -0,0 +1,46 @@
+//Problem Link : https://www.naukri.com/code360/problems/longest-common-substring_1214702?leftPanelTabValue=SUBMISSION
+
+//tabula memoization TC = O(m*n) and SC =  O(m*n)
+
+int LCSubStr(string &str1, string &str2){
+    int len1 = str1.length();
+    int len2 = str2.length();
+    vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+    int ans = 0;
+
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                ans = max(ans, dp[i][j]);
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    return ans;
+}
+
+int LCSubStr(string &str1, string &str2){
+    int len1 = str1.length();
+    int len2 = str2.length();
+    vector<int>prev(len1 + 1, 0);
+    vector<int>curr(len2 + 1, 0);
+    int ans = 0;
+
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                curr[j] = 1 + prev[j - 1];
+                ans = max(ans, curr[j]);
+            } else {
+                curr[j] = 0;
+            }
+        }
+        prev = curr;
+    }
+
+    return ans;
+}
+
