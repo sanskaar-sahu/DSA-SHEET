a10252fb561e65e3e2da906a6de7191ab0e8cc6e
sanskaar-sahu <sanskarsahu2095@gmail.com>
2025-08-17 22:47:03 +0530

    BFS or level order traversal for graph

---
+++ Graphs/bfs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public: 
+    vector<int> bfs(int start, vector<int> adj[], int n) {
+        vector<int> visited(n + 1, 0);  // n = number of nodes
+        queue<int> q;
+        visited[start] = 1;
+        q.push(start);
+        vector<int> res;
+
+        while (!q.empty()) {
+            int curr = q.front(); 
+            q.pop();
+            res.push_back(curr);
+
+            for (auto it : adj[curr]) {
+                if (!visited[it]) {
+                    visited[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return res; // ✅ return vector<int>
+    }
+};
