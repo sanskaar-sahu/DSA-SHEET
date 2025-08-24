a5f928af0297e66dc58701876b97ad0225e8f259
sanskaar-sahu <sanskarsahu2095@gmail.com>
2025-08-24 16:05:07 +0530

    Q-802. Find Eventual Safe States by REVERSING THE EDGES AND tropical sort

---
+++ Graphs/Tropical_sort/findEventualPath.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>>adj(n);  //reverse graph 
+
+        vector<int>indegree(n , 0);
+
+        for(int i =0 ; i < n ; i++){
+            for(auto it : graph[i]){
+                adj[it].push_back(i);
+                indegree[i]++;
+
+            }
+        }
+
+        queue<int>q;
+
+
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        vector<int>safenodes;
+
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+            safenodes.push_back(node);
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+
+        sort(safenodes.begin() , safenodes.end());
+
+        return safenodes;
+  
+        
+    }
+};
