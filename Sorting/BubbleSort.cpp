e35bd4650f12fbb7f9fde7c3ca4677a84f17c321
sanskaar-sahu <sanskarsahu2095@gmail.com>
2025-09-04 20:15:22 +0530

    QUICK SORT

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,40 +1,50 @@
-class Solution {
-    void swap(int &a , int &b){
+class Solution
+{
+    void swap(int &a, int &b)
+    {
         int temp = a;
         a = b;
         b = temp;
     }
-  public:
-    void bubbleSort(vector<int>& arr) {
+
+public:
+    void bubbleSort(vector<int> &arr)
+    {
         // code here
         int sz = arr.size();
-        
-        for(int i = 0 ; i < sz ; i++){
-            for(int j = i+1 ; j < sz ; j++){
-                if(arr[j] >= arr[j+1]){
-                    swap(arr[i] , arr[j]);
+
+        for (int i = 0; i < sz; i++)
+        {
+            for (int j = i + 1; j < sz; j++)
+            {
+                if (arr[j] >= arr[j + 1])
+                {
+                    swap(arr[i], arr[j]);
                 }
             }
         }
-        
     }
 
-void recurBubbleSort(vector<int>& arr, int n, int i, int j) {
-    // Base case: if all passes are done
-    if (i == n - 1) return;
+    void recurBubbleSort(vector<int> &arr, int n, int i, int j)
+    {
+        // Base case: if all passes are done
+        if (i == n - 1)
+            return;
 
-    // If we reached end of this pass, start next pass
-    if (j == n - i - 1) {
-        recurBubbleSort(arr, n, i + 1, 0);
-        return;
+        // If we reached end of this pass, start next pass
+        if (j == n - i - 1)
+        {
+            recurBubbleSort(arr, n, i + 1, 0);
+            return;
+        }
+
+        // Compare and swap
+        if (arr[j] > arr[j + 1])
+        {
+            swap(arr[j], arr[j + 1]);
+        }
+
+        // Move to next j in this pass
+        recurBubbleSort(arr, n, i, j + 1);
     }
-
-    // Compare and swap
-    if (arr[j] > arr[j + 1]) {
-        swap(arr[j], arr[j + 1]);
-    }
-
-    // Move to next j in this pass
-    recurBubbleSort(arr, n, i, j + 1);
-}
 };
---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    public:
+    
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[low];  // choosing first element as pivot
+        int i = low, j = high;
+
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) i++;
+            while (j >= low && arr[j] > pivot) j--;
+            if (i < j) swap(arr[i], arr[j]);
+        }
+        swap(arr[low], arr[j]);
+        return j;
+    }
+
+      void quickSort(vector<int>& arr , int low , int high){
+        if(low < high){
+            int partIdx = partition(arr , low , high);
+            
+            quickSort(arr , low , partIdx)
+            quickSort(arr , partIdx + 1 , high);
+        }
+      } 
+     
+}
