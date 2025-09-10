30138e907a05ec4bbca4c00aa84a7398f3da5e8a
sanskaar-sahu <sanskarsahu2095@gmail.com>
2025-09-10 11:57:27 +0530

    Q-796. Rotate String

---
+++ string/rotateString.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()) return false;  // ✅ fixed typo
+
+        string res(s.begin(), s.end());
+        res.insert(res.end(), s.begin(), s.end());     // ✅ duplicate s to make res twice
+
+        for(int i = 0 ; i < 2 * s.length(); i++){
+            string sub = res.substr(i, goal.length()); // ✅ added semicolon
+            if(sub == goal) return true;
+        }
+
+        return false;
+    }
+};
