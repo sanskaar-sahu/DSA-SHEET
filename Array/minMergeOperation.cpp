ab49998b2d278b3e6cf3a384a114056d31d68f45
sanskaar-sahu <sanskarsahu2095@gmail.com>
2026-02-09 08:22:30 +0530

    Min merge operation to make the array palindrom

---
+++ Array/minMergeOperation.cpp
@@ -0,0 +1,45 @@
+/**
+ * Find minimum number of merge operations to make an array palindrome
+Last Updated : 8 Jul, 2024
+Given an array of positive integers. We need to make the given array a 'Palindrome'. The only allowed operation is"merging" (of two adjacent elements). Merging two adjacent elements means replacing them with their sum. The task is to find the minimum number of merge operations required to make the given array a 'Palindrome'.
+
+To make any array a palindrome, we can simply apply merge operation n-1 times where n is the size of the array (because a single-element array is always palindromic, similar to single-character string). In that case, the size of array will be reduced to 1. But in this problem, we are asked to do it in the minimum number of operations.
+
+Example : 
+
+Input : arr[] = {15, 4, 15}
+Output : 0
+Array is already a palindrome. So we
+do not need any merge operation.
+Input : arr[] = {1, 4, 5, 1}
+Output : 1
+We can make given array palindrome with
+minimum one merging (merging 4 and 5 to
+make 9)
+Input : arr[] = {11, 14, 15, 99}
+Output : 3
+We need to merge all elements to make
+a palindrome.
+ */
+
+#include<bits/stdc++.h>
+int result(vector<int>& arr,int n){
+    int low = 0 , high = n - 1;
+    int cnt = 0;
+
+    while(low<= high){
+        if(arr[low] == high){
+            low++;
+            high--;
+        }else if(arr[low] < arr[high]){
+            low++; 
+            arr[low] = arr[low]+  arr[low-1];
+            cnt++;
+        }else{
+            high--;
+            arr[high] = arr[high] + arr[high -1];
+            cnt++;
+        }
+    }
+    return cnt;
+}
